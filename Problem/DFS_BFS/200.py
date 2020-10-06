85c3a7b86725a4bce14ccee728fe6bf2d861571a
Nam_Ssang <49119625+simian114@users.noreply.github.com>
2020-10-06 23:53:30 +0900

    책 32번, 섬의 개수

--- Problem/DFS_BFS/200.py
+++ Problem/DFS_BFS/200.py
@@ -12,6 +12,7 @@ class Solution:
             dfs(i, j + 1)
         
         count = 0
+        # dfs 시작
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
